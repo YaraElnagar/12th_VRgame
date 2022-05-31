f9bda0c7ad38e05f260ee80012af72ea84c1f354
Yara Ayman <yara1212egy@gmail.com>
2022-05-31 22:54:30 +0200

    first version

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+    private int score = 0;
+    public Text scoreText;
+    public GameObject level2, level3;
+    public float speed = 5;
+    public GameObject PlayerHands;
+    public GameObject ObjectInMyHands;
+    private bool carry = false;
+    public AudioClip collectSound, targetSound;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            this.transform.position += Camera.main.transform.forward * speed * Time.deltaTime;
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ball" && !carry)
+        {
+                other.gameObject.transform.SetParent(PlayerHands.transform);
+                other.gameObject.transform.position = PlayerHands.transform.position;
+                ObjectInMyHands = other.gameObject;
+                carry = true;
+                other.gameObject.GetComponent<Animator>().enabled = false;
+                Camera.main.GetComponent<AudioSource>().PlayOneShot(collectSound);
+        }
+
+        if (other.gameObject.tag == "Target" && carry)
+        {
+                other.gameObject.SetActive(false);
+                ObjectInMyHands.transform.SetParent(null);
+                ObjectInMyHands.transform.position = other.gameObject.transform.position + new Vector3(0,0.5f,0);
+                ObjectInMyHands.tag = "BallDown";
+                ObjectInMyHands.GetComponent<Renderer>().material.color = Color.blue;
+                carry = false;
+                score++;
+                Camera.main.GetComponent<AudioSource>().PlayOneShot(targetSound);
+                scoreText.text = "Score: " + score;
+                
+        }
+
+        if (score == 11)
+        {
+            level2.gameObject.GetComponent<Animator>().enabled = true;
+        }
+
+        if (score == 22)
+        {
+            level3.gameObject.GetComponent<Animator>().enabled = true;
+        }
+
+        if (score == 33)
+        {
+            scoreText.text = "We have a WINNER!";
+        }
+    }
+
+}
---
+++ Assets/Scripts/VRGaze.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class VRGaze : MonoBehaviour
+{
+    private bool gvrStatus =false;
+    public Image gazeImage;
+    public float totalTime = 2;
+    private float currentGazeTimer = 0;
+
+    private Ray ray;
+    private float rayDistance = 3;
+    private RaycastHit hit;
+
+    public GameObject teleport1, teleport2;
+
+
+    public void GVROn()
+    {
+        gvrStatus = true;
+    }
+
+
+    public void GVROFF()
+    {
+        gvrStatus = false;
+        currentGazeTimer = 0;
+        gazeImage.fillAmount = 0;
+    }
+
+    private void Update()
+    {
+        ray = Camera.main.ViewportPointToRay(new Vector3 (0.5f,0.5f,0.5f));
+        if (Physics.Raycast(ray, out hit, rayDistance))
+        {
+            if (gvrStatus)
+                {
+                    currentGazeTimer += Time.deltaTime;
+                    gazeImage.fillAmount = currentGazeTimer / totalTime;
+
+                    if (gazeImage.fillAmount == 1 && hit.transform.tag =="Ball")
+                    {
+                        hit.transform.GetComponent<Animator>().enabled = !hit.transform.GetComponent<Animator>().enabled;
+                        GVROFF();
+                    }
+
+                     if (gazeImage.fillAmount == 1 && hit.transform.tag =="teleport1")
+                    {
+                        this.transform.position = teleport2.transform.position;
+                        GVROFF();
+                    }
+
+                    if (gazeImage.fillAmount == 1 && hit.transform.tag =="teleport2")
+                    {
+                        this.transform.position = teleport1.transform.position;
+                        GVROFF();
+                    }
+                }
+        }
+
+        
+    }
+
+}
